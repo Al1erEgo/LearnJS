7203f3cfef278933ba4f0455ada836bb11213b13
al1erego <xz.fanat1k@gmail.com>
2022-11-25 00:26:02 +0100

    25.11

--- _CodeWars/How many are smaller than me II.js
+++ _CodeWars/How many are smaller than me II.js
@@ -18,19 +18,15 @@ function smaller(arr) {
           return arr.reduceRight( (accumulator, item, index, array) => {
             let counter = 0;
             for (i = index; i < arr.length; i++) {
-                console.log(array[i+1])
                 if (isNaN(array[i+1])) {
                     accumulator[index] = 0;
-                    console.log('a')
                     break;
                 }
                 if (item == array[i+1]) {
                     counter = counter + accumulator[i+1];
                     accumulator[index] = counter;
-                    console.log('b')
                     break;
                 } else if (item > array[i+1]) {
-                    console.log('c')
                     ++counter;
                 }
             }
@@ -38,5 +34,29 @@ function smaller(arr) {
             return accumulator;
           } , []);
          }
-   
-   console.log (smaller3([1, 2, 0]))
+
+function smaller4 (arr) {
+    let accumulator = {
+        sum: [5, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+        0: () => this.sum[0],
+        1 ()  {this[0] + this.sum[1]},
+        // 2 : this[1] + this.sum[2],
+        // 3 : this[2] + this.sum[3],
+        // 4 : this[3] + this.sum[4],
+        // 5 : this[4] + this.sum[5],
+        // 6 : this[5] + this.sum[6],
+        // 7 : this[6] + this.sum[7],
+        // 8 : this[7] + this.sum[8], 
+        // 9 : this[8] + this.sum[9],
+    }
+    return accumulator["0"]()
+    console.log(accumulator.sum)
+
+
+
+
+
+
+}
+console.log (smaller4([5, 4, 7, 9, 2, 4, 1, 4, 5, 6]))
+   console.log (smaller4([5, 4, 7, 9, 2, 4, 1, 4, 5, 6]))
